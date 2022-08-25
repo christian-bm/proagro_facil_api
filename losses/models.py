1b9820a9b7f4dac06755a735643ccfa61257ba61
Christian <christianbm1998@gmail.com>
2022-08-25 16:19:33 -0400

    feat: start project, create app losses and create model loss

---
+++ losses/models.py
@@ -0,0 +1,30 @@
+import uuid
+
+from django.db import models
+
+
+class EventTypeChoices(models.TextChoices):
+    CHUVA = "Chuva excessiva"
+    GEADA = "Geada"
+    GRANIZO = "Granizo"
+    SECA = "Seca"
+    VENDAVAL = "Vendaval"
+    RAIO = "Raio"
+    OUTRO = "Outro"
+
+
+class Loss(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False, unique=True
+    )
+    first_name = models.CharField(max_length=125, null=False)
+    last_name = models.CharField(max_length=125, null=False)
+    email = models.EmailField(max_length=255, null=False)
+    cpf = models.CharField(max_length=11, null=False)
+    latitude = models.CharField(max_length=14, null=False)
+    longitude = models.CharField(max_length=14, null=False)
+    farming_type = models.CharField(max_length=125, null=False)
+    harvest_date = models.DateField()
+    event_type = models.CharField(
+        max_length=20, choices=EventTypeChoices.choices, default=EventTypeChoices.OUTRO
+    )
